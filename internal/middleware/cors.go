8c999fe582560c897a664dbc89c0d3fd677b96e8
Vincent Nguyen <vinh.nglx@gmail.com>
2025-08-18 22:07:17 +1200

    Add CORS middleware for handling cross-origin requests

---
+++ internal/middleware/cors.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CORS() gin.HandlerFunc {
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Accept", "Authorization"}
+
+	return cors.New(config)
+}
